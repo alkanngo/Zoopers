1ff2ac7dc8042e3ae25f3859f170fed6671f302e
al <aladdin.kanngo@hotmail.com>
2020-01-10 23:57:06 +0100

    Add images small change in structure of typography

--- app/styles/typography.js
+++ app/styles/typography.js
@@ -1,8 +1,11 @@
-export const fonts = {
+export const size = {
     xsm: 12,
     sm: 14,
-    md: 18,
+    md: 20,
     lg: 28,
-    xlg: 35,
+    xlg: 35
+}
+
+export const font = {
     primary: 'Montserrat'
 }
