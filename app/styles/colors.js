cf225ad831f68e1bd042c48487e3172cb82b8734
al <aladdin.kanngo@hotmail.com>
2020-02-11 18:55:15 +0100

    Adjustments to styles

--- app/styles/colors.js
+++ app/styles/colors.js
@@ -1,6 +1,9 @@
-export const colors  = {
+export const name  = {
     primary: '#262626',
     secondary: '#FA7A35',
     background: '#4A4B4E',
-    error: "#8A2BE2"
+    error: "#8A2BE2",
+    primary60Opacity: "rgba(38, 38, 38, 0.6)",
+    secondary60Opacity:"rgba(250, 122, 53, 0.4)",
+
 }
--- app/styles/typography.js
+++ app/styles/typography.js
@@ -1,9 +1,9 @@
 export const size = {
     xsm: 12,
     sm: 14,
-    md: 20,
-    lg: 28,
-    xlg: 35
+    md: 18,
+    lg: 22,
+    xlg: 28
 }
 
 export const font = {
