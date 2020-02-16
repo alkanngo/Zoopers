97274a3efb126fade0e6673933a7a3191b937be7
al <aladdin.kanngo@hotmail.com>
2020-02-16 21:09:48 +0100

    Add logic to see if user is logged in or not

--- app/screens/LoadingScreen.tsx
+++ app/screens/LoadingScreen.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, ImageBackground } from 'react-native';
 import { Container, Spinner} from "native-base";
+import firebase from "firebase";
 
-function LoginScreen() {
+function LoadingScreen(props) {
 
+    useEffect(() => {
+        checkIfLoggedIn();
+    });
+
+    const checkIfLoggedIn = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                props.navigation.navigate("Home")
+            }
+            else {
+                props.navigation.navigate("Login")
+            }
+        })
+    }
 
     return(
         <Container>
