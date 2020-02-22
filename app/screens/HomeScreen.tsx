0f165dbd7e3509b18bfea7524a4452628f610db3
al <aladdin.kanngo@hotmail.com>
2020-02-22 19:45:27 +0100

    Implement custom HoopButton component

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Slider } from "react-native";
 import {Colors} from "../styles";
-import { Button } from "native-base";
+import { Button, Container, Content } from "native-base";
 import firebase from "firebase";
-
+import HoopButton from "./../components/HoopButton";
 
 function HomeScreen (props) {
 
@@ -17,22 +17,42 @@ function HomeScreen (props) {
   }
 
   return (
-    <View>
-      <Text>HEMMASKÄRM</Text>
-      <Button onPress={navigateToMaps}>
-        <Text>Maps</Text>
-      </Button>
-      <Button danger onPress={logoutUser}>
-        <Text>Logout</Text>
-      </Button>
-    </View>
+    <Container style={styles.container}>
+      <Content style={styles.content}>
+        <View style={styles.slider}>
+          <Slider />
+        </View>
+        <HoopButton 
+          value="Locate"
+          onPress={navigateToMaps}
+        />
+        <Button danger onPress={logoutUser}>
+          <Text>Logout</Text>
+        </Button>
+      </Content>
+    </Container>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    color: Colors.name.secondary,
+    backgroundColor: Colors.name.primary,
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
   },
+  content: {
+    width: "100%",
+    height: 200,
+    alignContent: "center",
+  },
+  slider: {
+    backgroundColor: Colors.name.secondary
+  },
+  button: {
+
+  }
 
 });
 
