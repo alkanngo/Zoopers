fb0d59b6f9f16bd30bcb4c00cb21c5f0036365ac
al <aladdin.kanngo@hotmail.com>
2020-02-26 18:14:14 +0100

    Add small logo to home-screen

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -9,14 +9,33 @@ export const BigLogo:React.FC = () => {
   );
 }
 
+export const MiniLogo:React.FC = () => {
+  return (
+    <View style={styles.miniContainer}>
+      <Image
+        style={styles.mini}
+        source={require("./../../assets/miniLogo.png")}
+      />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   logoContainer:{
-    marginTop: 70,
-    marginBottom: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: 220
+  },
+  miniContainer:{
     width: "100%",
   },
   logo:{
     width: 360,
     height: 118,
   },
+  mini: {
+    width: 200, 
+    height: 44,
+  }
 });
